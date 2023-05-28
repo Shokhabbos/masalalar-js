a549cd19f59221823872a9d37145adc3f9725270
shokhabbos <urinovs999@gmail.com>
2023-05-28 18:31:24 +0500

    added

--- my.js
+++ my.js
@@ -1,3 +1,13 @@
+const reverseSeq = n => {
+  let arr =[]
+  for(let i = n; i > 0; i--){
+     console.log(arr.push(i), i, n);
+  }
+  return arr
+};
+console.log(reverseSeq(5));
+
+
 function findNextSquare(sq) {
   // Return the next square if sq is a perfect square, -1 otherwise
   if(sq**0.5 % 1 === 0){
