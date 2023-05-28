377c37fa1bfce7ae052e5390713d850ca1d8f9d0
shokhabbos <urinovs999@gmail.com>
2023-05-28 18:25:18 +0500

    findNextSquare

--- my.js
+++ my.js
@@ -1,8 +1,7 @@
-const reverseSeq = n => {
-  let arr =[]
-  for(let i = n; i > 0; i--){
-     console.log(arr.push(i), i, n);
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  if(sq**0.5 % 1 === 0){
+    return (sq**0.5 +1)**2
   }
-  return arr
-};
-console.log(reverseSeq(5));
+  return -1;
+}
