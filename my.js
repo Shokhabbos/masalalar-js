85b3f31cb00ad48968504b99629be9b59312760e
shokhabbos <urinovs999@gmail.com>
2023-07-22 13:35:53 +0500

    food

--- my.js
+++ my.js
@@ -285,24 +285,24 @@ console.log(checkTypeOfNumber([1,2,3]));
 // //     return true;
 // //   };
 
-// const isEqualObjs = (obj1, obj2) => {
-//     const firstObj = Object.entries(obj1);
-//     const secondObj = Object.entries(obj2);
+const isEqualObjs = (obj1, obj2) => {
+    const firstObj = Object.entries(obj1);
+    const secondObj = Object.entries(obj2);
   
-//     if (firstObj.length !== secondObj.length) {
-//       return false;
-//     }
+    if (firstObj.length !== secondObj.length) {
+      return false;
+    }
   
-//     for (let i = 0; i < firstObj.length; i++) {
-//       const [key1, val1] = firstObj[i];
-//       const [key2, val2] = secondObj.find(([key]) => key === key1) || [];
-//       if (val1 !== val2) {
-//         return false;
-//       }
-//     }
+    for (let i = 0; i < firstObj.length; i++) {
+      const [key1, val1] = firstObj[i];
+      const [key2, val2] = secondObj.find(([key]) => key === key1) || [];
+      if (val1 !== val2) {
+        return false;
+      }
+    }
   
-//     return true;
-//   }; 
+    return true;
+  }; 
 
 // const food = ['🍕', '🍫', '🥑', '🍔'];
 // const info = { favoriteFood: food[0] };
