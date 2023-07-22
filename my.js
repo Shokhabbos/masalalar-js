dc76a6a04326a0d8d938a53c2d0166e26816ebf9
shokhabbos <urinovs999@gmail.com>
2023-07-22 18:18:53 +0500

    likes

--- my.js
+++ my.js
@@ -1,4 +1,8 @@
-function descendingOrder(n){
-  //...
-  return Number(n.toString().split('').sort().reverse().join(''))
+function likes(names) {
+  // TODO
+  if(names.length === 0) return "no one likes this";
+  if(names.length === 1) return `${names[0]} likes this`
+  if(names.length === 2) return `${names[0]} and ${names[1]} like this`
+  if(names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  if(names.length >= 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
 }
