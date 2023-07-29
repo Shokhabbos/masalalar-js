d8ebecd8ef7a459f1dbd22af41d4eb803c47fc29
shokhabbos <urinovs999@gmail.com>
2023-07-29 13:48:35 +0500

    greeting

--- my.js
+++ my.js
@@ -1,20 +1,28 @@
-var Calculator = {
-       add(x, y){
-            return x+y;
-        },
-        subtract(x, y){
-            return x-y;
-        },
-        multiply(x, y){
-            return x*y;
-        },
-     
-        divide(x, y){
-            if(y !== 0){
-                return x/y;
-            }else{
-                return false
-            }
-            
-        }
-};
+function greet(language) {
+    let database = {
+      english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+   }
+      if(database[language] !== undefined){
+        
+        return database[language]
+      }else{
+        return 'Welcome'
+    }
+      
+  }
